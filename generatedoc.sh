37269faf137c7e4a79d704b62b230ba5cf24c867
szilidaniel <34722259+szilidaniel@users.noreply.github.com>
2019-11-23 19:46:59 +0100

    pdf

---
+++ generatedoc.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install doxygen
+sudo apt-get install graphviz
+doxygen -g
+doxygen Doxyfile
+sudo apt-get install texlive-full
+sudo apt-get install texmaker
+latex make
