dee3cc9e5629f152c7b9cf217dbdec572318331c
rajfruzsi <34722134+rajfruzsi@users.noreply.github.com>
2019-12-03 11:36:43 +0100

    masik szar

--- cmd.h
+++ cmd.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -23,13 +24,22 @@ class Dictionary {
 private:
 	vector<Pair> system;
 	vector<FileDescriptor> fileDescriptorVector;
-	vector<string> commands = { "mkdir","exit","ls","cd","rm","touch" };
+	vector<string> commands = { "mkdir","exit","ls","cd","rm","touch", "echo" };
+	void deleteRecursively(string toDelete, string currentFolder);
+	bool searchFile(string fileName, string filePath);
+	bool searchDirectory(string forder, string subfolder);
+	string getLastElement(string fullPath);
+	string getPath(string fullPath, string currentFolder);
+	string getParentPath(string fullPath);
+	string convertRelativeToAbsolutePath(string relativePath, string currentFolder);
+	void moveFile(string sourcePath, string sourceLastElement, string destPath, string destLastElement);
+	void moveDirectory(string sourcePath, string sourceLastElement, string destPath, string destLastElement);
+
 public:
 	void mkdir(string dirName, string currentFolder);
 	void ls(string currentFolder);
 	string cd(string dirName, string currentFolder);
 	void rm(string dirName, string currentFolder);
-	void deleteRecursively(string toDelete, string currentFolder);
 	void rmForce(string dirName, string currentFolder);
 	void touch(string fileName, string currentFolder);
 	void echo(string fileContent, string fileName, string currentFolder);
@@ -38,8 +48,11 @@ public:
 	void splitString(string& str, vector<string>& out, string delim);
 	void addPairToVector(string folder, string subfolder);
 	void splitFolderPath(string line);
-	bool isNotValid(string dirName);
+	bool isValid(string dirName);
 	bool validcommand(string command);
+	void splitDirNameAndPath(string dirname, string CurrentFolder, vector<string>path);
+	string splitCurrentFolder(string CurrentFolder);
+	void mv(string currentFolder, string source, string dest);
 };
 
 #endif
